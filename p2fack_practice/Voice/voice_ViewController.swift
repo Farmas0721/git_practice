0b56ff3227a5d9964794ddeb7a11072a2b52064d
山田楓也 <fuya0713@gmail.com>
2019-02-10 16:35:20 +0900

    練習用画面遷移のdodaisakusei

---
+++ p2fack_practice/Voice/voice_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  voice_ViewController.swift
+//  p2fack_practice
+//
+//  Created by Fuuya Yamada on 2019/02/10.
+//  Copyright © 2019 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class voice_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
