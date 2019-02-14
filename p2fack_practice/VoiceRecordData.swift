2b8773186bc22f8b0a648424768dd15bb4a0048e
中村　朝陽 <>
2019-02-14 17:06:43 +0900

    データクラス作った

---
+++ p2fack_practice/VoiceRecordData.swift
@@ -0,0 +1,13 @@
+//
+//  VoiceRecordData.swift
+//  p2fack_practice
+//
+//  Created by 中村　朝陽 on 2019/02/14.
+//  Copyright © 2019年 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class VoiceRecordData: NSObject {
+
+}
