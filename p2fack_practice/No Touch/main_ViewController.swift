8348446f1b851afedb0b5a560d8a2c37e8e6aa9c
中村　朝陽 <>
2019-02-14 16:46:13 +0900

    main_VeiwControllerを追加

---
+++ p2fack_practice/No Touch/main_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  main_ViewController.swift
+//  p2fack_practice
+//
+//  Created by 中村　朝陽 on 2019/02/14.
+//  Copyright © 2019年 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class main_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
