da66f979881de56f184ee1f9b6cba890c0094d5c
MasatakaKudou <b1017027@fun.ac.jp>
2019-02-16 10:50:59 +0900

    カレンダーを表示させた

---
+++ p2fack_practice/Calender/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  p2fack_practice
+//
+//  Created by codepro on 2019/02/15.
+//  Copyright © 2019年 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+}
---
+++ p2fack_practice/Calender/DateManager.swift
@@ -0,0 +1,13 @@
+//
+//  DateManager.swift
+//  p2fack_practice
+//
+//  Created by codepro on 2019/02/15.
+//  Copyright © 2019年 Fuuya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class DateManager: NSObject {
+
+}
